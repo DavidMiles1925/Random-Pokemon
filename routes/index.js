34020eaf51984db7c95a5d3d96f3d0970fb1a984
David Miles <davidmiles1925@gmail.com>
2023-08-18 01:27:38 -0500

    outlined routes

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,10 @@
 const router = require("express").Router();
+const pokeRouter = require("./pokemon");
+
+router.use("/pokemon", pokeRouter);
+
+router.use((req, res, next) => {
+  next(new NotFoundError("Route not found"));
+});
 
 module.exports = router;
---
+++ utils/errorConstants.js
@@ -0,0 +1,8 @@
+module.exports.BAD_REQUEST_ERROR = 400;
+module.exports.UNAUTHORIZED__ERROR = 401;
+module.exports.FORBIDDEN_ERROR = 403;
+module.exports.NOT_FOUND_ERROR = 404;
+module.exports.CONFLICT_ERROR = 409;
+module.exports.DEFAULT_ERROR = 500;
+
+module.exports.USER_OK = 200;
